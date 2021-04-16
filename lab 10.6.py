9258fcd9fde0b4985e425ed7895ffdc8ba9b2400
Eldar Kurbanov <EldarK@outlook.com>
2021-04-16 19:22:07 +0300

    Remove second plot

--- lab 10.6.py
+++ lab 10.6.py
@@ -5,6 +5,5 @@ import scipy.spatial
 points = numpy.random.rand(10, 2)
 vor = scipy.spatial.Voronoi(points)
 scipy.spatial.voronoi_plot_2d(vor)
-scipy.spatial.voronoi_plot_2d(vor, show_vertices=False, line_colors='orange',
-                              line_width=3, line_alpha=0.6, point_size=2)
+
 matplotlib.pyplot.show()
